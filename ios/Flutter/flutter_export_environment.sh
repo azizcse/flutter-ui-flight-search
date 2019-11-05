6b92ad3785bd8b89dc30485f7c96a9dabf00ffc8
Aziz <azizul@w3engineers.com>
2019-11-05 18:16:21 +0600

    First

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/w3e96/Programs/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/w3e96/projects/personal/flutter_ui_challenge_flight_search"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/w3e96/Programs/flutter/bin/cache/artifacts/engine/ios"
